78ecc5d448c65278938f31ad60ccf83aab460431
jerryskye <jaroslaw.siedlarz@netguru.pl>
2019-04-23 12:02:57 +1000

    Add VimMode spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,10 @@
 hs.loadSpoon('ControlEscape'):start()
+local vim_mode = hs.loadSpoon('VimMode')
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
+end)
+
+hs.hotkey.bind({'ctrl'}, '\\', function()
+  vim_mode:enter()
 end)
