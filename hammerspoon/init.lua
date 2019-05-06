da960647d393581409c4ee9b582f66cc7bb914e7
jerryskye <jarek@siedlarz.com>
2019-05-06 22:36:09 +0200

    Add hotkeys for focusing / launching specific apps

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,3 +8,19 @@ end)
 hs.hotkey.bind({'ctrl'}, '\\', function()
   vim_mode:enter()
 end)
+
+hs.hotkey.bind({'cmd', 'ctrl'}, 'M', function()
+  hs.application.launchOrFocus('Messages')
+end)
+
+hs.hotkey.bind({'cmd', 'ctrl'}, 'T', function()
+  hs.application.launchOrFocus('iTerm')
+end)
+
+hs.hotkey.bind({'cmd', 'ctrl'}, 'W', function()
+  hs.application.launchOrFocus('Firefox')
+end)
+
+hs.hotkey.bind({'cmd', 'ctrl'}, 'S', function()
+  hs.application.launchOrFocus('Slack')
+end)
