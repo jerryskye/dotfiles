75a88e955e3a5b06ed2cf4055be88bd7fb54b3d6
jerryskye <jarek@siedlarz.com>
2019-05-11 15:39:19 +0200

    Add window layout managing

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,37 @@
 hs.loadSpoon('ControlEscape'):start()
 local vim_mode = hs.loadSpoon('VimMode')
 
+local slack_window_layout = {'Slack', 'move 0.0,0.0,0.8,1 Color'}
+local itunes_window_layout = {{iTunes = {allowTitles = 'iTunes'}}, 'move 0.0,0.0,0.92,1 Color'}
+local itunes_mini_player_layout = {{iTunes = {allowTitles = 'Mini Player'}}, 'move 0.8,0.0,0.8,1 Color'}
+local imessage_window_layout = {'Messages', 'move 0.2,0.0,0.8,0.66 Color'}
+local window_layouts = {
+  hs.window.layout.new({
+    slack_window_layout,
+    itunes_mini_player_layout,
+    itunes_window_layout,
+    imessage_window_layout,
+    {'iTerm2', 'fullscreen Color'},
+    {'Firefox', 'fullscreen Color'}
+  }):setScreenConfiguration({Color = true, MP59G = false, DELL = false}),
+  hs.window.layout.new({
+    slack_window_layout,
+    itunes_mini_player_layout,
+    itunes_window_layout,
+    imessage_window_layout,
+    {'iTerm2', 'fullscreen MP59G'},
+    {'Firefox', 'fullscreen MP59G'}
+  }):setScreenConfiguration({Color = true, MP59G = true, DELL = false}),
+  hs.window.layout.new({
+    slack_window_layout,
+    itunes_mini_player_layout,
+    itunes_window_layout,
+    imessage_window_layout,
+    {'iTerm2', 'fullscreen DELL'},
+    {'Firefox', 'fullscreen MP59G'}
+  }):setScreenConfiguration({Color = true, MP59G = true, DELL = true})
+}
+
 hs.hotkey.bind({'cmd', 'ctrl'}, 'R', function()
   hs.reload()
 end)
@@ -28,4 +59,10 @@ end)
 hs.hotkey.bind({'cmd', 'ctrl'}, 'P', function()
   local time_left = hs.itunes.getDuration() - hs.itunes.getPosition()
   hs.timer.doAfter(time_left, hs.itunes.pause)
+end)
+
+hs.hotkey.bind({'cmd', 'ctrl'}, 'L', function()
+  for _,layout in pairs(window_layouts) do
+    layout:apply()
+  end
 end)
