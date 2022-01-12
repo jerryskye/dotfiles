b2c4e8c4ff73400bd1fe9e914f73880b10b9ec37
jerryskye <jarek@siedlarz.com>
2022-01-12 20:16:47 +0100

    Add debounce_text_changes to lsp config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -77,6 +77,9 @@ local servers = { 'solargraph' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
   }
 end
 
