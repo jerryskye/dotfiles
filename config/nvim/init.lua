0434a86a8248e7dd8ec4352e2c577195717cbbb6
jerryskye <jarek@siedlarz.com>
2022-01-14 00:36:40 +0100

    Disable nvim lsp diagnostics in insert mode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -83,4 +83,10 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    update_in_insert = false,
+  }
+)
+
 require('lspfuzzy').setup {}
