38bfc29b64c59e21b216e88c8b0b1ca6fb9f6fbd
jerryskye <jarek@siedlarz.com>
2022-07-18 13:34:53 +0200

    Replace Music.app with Spotify in hammerspoon layout

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -48,17 +48,15 @@ end
 hs.hotkey.bind({'cmd', 'ctrl'}, 'L', function()
   local screens = screens_table()
 
-  local mini_player_width = 288
-  local screen_width = screens[1]:frame().w
-  local mini_player_unit_w = mini_player_width / screen_width
-  local mini_player_unit_x1 = 1 - mini_player_unit_w
+  local spotify_bar_height = 90
+  local with_spotify_bar_visible = screens[2]:frame()
+  with_spotify_bar_visible.h = with_spotify_bar_visible.h - spotify_bar_height
+
   hs.layout.apply({
     {"Firefox", nil, screens[1], hs.layout.maximized, nil, nil},
-    {"Music", "Mini Player", screens[2], {mini_player_unit_x1, 0, mini_player_unit_w, 1}, nil, nil},
-    {"Music", "Music", screens[2], '0,0, 0.92,1', nil, nil},
-    {"Messages", nil, screens[2], {x1=0.2, y1=0, x2=mini_player_unit_x1, y2=0.66}, nil, nil},
-    {"Slack", nil, screens[1], {x1=0, y1=0, x2=mini_player_unit_x1, y2=1}, nil, nil},
-    {"Discord", nil, screens[2], {x1=0, y1=0, x2=mini_player_unit_x1, y2=1}, nil, nil},
+    {"Spotify", nil, screens[2], hs.layout.maximized, nil, nil},
+    {"Messages", nil, screens[2], {x1=0.2, y1=0, w=0.6, h=0.66}, nil, nil},
+    {"Slack", nil, screens[2], nil, with_spotify_bar_visible, nil, options={absolute_x=true, absolute_y=true}},
     {"Alacritty", nil, screens[1], hs.layout.maximized, nil, nil}
   })
 end)
