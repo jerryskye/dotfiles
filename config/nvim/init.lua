eaffb345c070374f84c5e09e23955565c2aab684
jerryskye <jarek@siedlarz.com>
2025-11-04 02:01:27 +0100

    Switch from packer.nvim to lazy.nvim
    https://github.com/folke/lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -87,16 +87,35 @@ vim.api.nvim_create_user_command('Rgaf', function(opts)
   vim.cmd('call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case --hidden --no-ignore --fixed-strings ' .. vim.api.nvim_call_function('shellescape', {opts.args}) .. '", 1, fzf#vim#with_preview(), ' .. (opts.bang and '1' or '0') .. ')')
 end, { nargs = '*', bang = true })
 
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'chriskempson/base16-vim'
-  use 'junegunn/fzf'
-  use 'junegunn/fzf.vim'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('gitsigns').setup {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+      {
+      'chriskempson/base16-vim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme base16-tomorrow-night")
+      end
+    },
+    'junegunn/fzf',
+    { 'junegunn/fzf.vim', dependencies = { 'junegunn/fzf' } },
+    {
+      'lewis6991/gitsigns.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      opts = {
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
 
@@ -136,87 +155,90 @@ require('packer').startup(function()
           map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
         end
       }
-    end
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'ojroques/nvim-lspfuzzy'
-  use {
-    'francoiscabrol/ranger.vim',
-    requires = 'rbgrouleff/bclose.vim'
-  }
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'tpope/vim-bundler'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-dispatch'
-  use 'elixir-editors/vim-elixir'
-  use 'junegunn/vim-emoji'
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-projectionist'
-  use 'tpope/vim-rails'
-  use 'vim-ruby/vim-ruby'
-  use 'slim-template/vim-slim'
-  use 'hashivim/vim-terraform'
-  use 'vim-test/vim-test'
-  use 'tpope/vim-unimpaired'
+    },
+    {
+      'neovim/nvim-lspconfig',
+      config = function()
+        -- Use an on_attach function to only map the following keys
+        -- after the language server attaches to the current buffer
+        local on_attach = function(_, bufnr)
+          local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+          local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  use { 'edkolev/tmuxline.vim', opt = true }
-  use { 'leafgarland/typescript-vim', opt = true }
-  use { 'posva/vim-vue', opt = true }
-end)
+          -- Enable completion triggered by <c-x><c-o>
+          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+          -- Mappings.
+          local opts = { noremap=true }
 
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+          buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+          buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+          buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+          buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+          buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+          buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+          buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+          buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+          buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+          buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+          buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+          buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+          buf_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+        end
 
-  -- Mappings.
-  local opts = { noremap=true }
+        -- Use a loop to conveniently call 'setup' on multiple servers and
+        -- map buffer local keybindings when the language server attaches
+        local servers = { 'solargraph' }
+        for _, lsp in ipairs(servers) do
+          vim.lsp.config(lsp , {
+            on_attach = on_attach,
+            flags = {
+              debounce_text_changes = 150,
+            }
+          })
+        end
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-end
-
-vim.cmd("colorscheme base16-tomorrow-night")
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'solargraph' }
-for _, lsp in ipairs(servers) do
-  vim.lsp.config(lsp , {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  })
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    update_in_insert = false,
-  }
-)
-
-require('lspfuzzy').setup {}
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+          vim.lsp.diagnostic.on_publish_diagnostics, {
+            update_in_insert = false,
+          }
+        )
+      end
+    },
+    {
+      'ojroques/nvim-lspfuzzy',
+      config = function()
+        require('lspfuzzy').setup {}
+      end
+    },
+    {
+      'francoiscabrol/ranger.vim',
+      dependencies = { 'rbgrouleff/bclose.vim' }
+    },
+    'vim-airline/vim-airline',
+    'vim-airline/vim-airline-themes',
+    'tpope/vim-bundler',
+    'tpope/vim-commentary',
+    'tpope/vim-dispatch',
+    'elixir-editors/vim-elixir',
+    'junegunn/vim-emoji',
+    'tpope/vim-endwise',
+    'tpope/vim-fugitive',
+    'tpope/vim-projectionist',
+    'tpope/vim-rails',
+    'vim-ruby/vim-ruby',
+    'slim-template/vim-slim',
+    'hashivim/vim-terraform',
+    'vim-test/vim-test',
+    'tpope/vim-unimpaired',
+    'edkolev/tmuxline.vim',
+    { 'leafgarland/typescript-vim', ft = "typescript" },
+    { 'posva/vim-vue', ft = "vue" },
+  },
+  checker = { enabled = true }
+})
